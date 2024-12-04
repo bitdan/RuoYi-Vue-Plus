19a60bc6880287534f0eeac4334e8497967bb96b
bitdan <abiasnip@gmail.com>
2024-12-04 16:17:33 +0800

    :sparkles: modify k8s.sh -3

--- script/shell/k8s.sh
+++ script/shell/k8s.sh
@@ -57,7 +57,12 @@ echo "配置网桥的 IPv4 流量"
 cat > /etc/sysctl.d/k8s.conf << EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
+
 EOF
+
+echo "net.ipv4.ip_forward = 1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+
 
 # 应用 sysctl 设置
 sysctl --system
@@ -85,7 +90,7 @@ systemctl restart network
 ip addr | grep ${NEW_IP}
 
 # 添加 Kubernetes 官方仓库
-cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+cat <<EOF > kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64
@@ -95,6 +100,7 @@ repo_gpgcheck=0
 gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
 EOF
 
+mv kubernetes.repo /etc/yum.repos.d/
 
 # 安装 kubernetes 组件
 yum install -y kubelet-1.23.0-0 kubectl-1.23.0-0 kubeadm-1.23.0-0
@@ -102,65 +108,4 @@ yum install -y kubelet-1.23.0-0 kubectl-1.23.0-0 kubeadm-1.23.0-0
 systemctl enable kubelet
 systemctl start kubelet
 echo "kubelet install ok"
-
-# 查找 10-kubeadm.conf 文件
-KUBEADM_CONF=$(find / -name 10-kubeadm.conf 2>/dev/null)
-
-# 检查是否找到文件
-if [ -z "$KUBEADM_CONF" ]; then
-    echo "未找到 10-kubeadm.conf 文件"
-    exit 1
-fi
-
-PUBLIC_IP=${HOST_IPS[$HOSTNAME]}
-
-# 备份原始配置文件
-cp "$KUBEADM_CONF" "${KUBEADM_CONF}.bak"
-
-# 倒序读取并修改最后一个 ExecStart= 行
-tac "$KUBEADM_CONF" | sed '/ExecStart=/s/$/ --node-ip='"$PUBLIC_IP"'/' | tac > "$KUBEADM_CONF"
-
-
-# 重新加载 systemd 配置
-systemctl daemon-reload
-
-echo "10-kubeadm.conf modify ok"
-
-# Master 节点特殊处理
-if [ "$HOSTNAME" == "master" ]; then
-    # Master 节点初始化
-    kubeadm init \
-    --apiserver-advertise-address=$PUBLIC_IP \
-    --kubernetes-version v1.23.0 \
-    --control-plane-endpoint=$PUBLIC_IP \
-    --service-cidr=10.96.0.0/12 \
-    --pod-network-cidr=10.244.0.0/16 \
-    --v=5
-
-    # 配置 kubectl
-    mkdir -p $HOME/.kube
-    cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-    chown $(id -u):$(id -g) $HOME/.kube/config
-
-
-    # API Server 配置文件路径
-    APISERVER_CONFIG="/etc/kubernetes/manifests/kube-apiserver.yaml"
-
-    # 检查文件是否存在
-    if [ ! -f "$APISERVER_CONFIG" ]; then
-        echo "kube-apiserver.yaml 文件不存在"
-        exit 1
-    fi
-
-    # 备份原始配置文件
-    cp "$APISERVER_CONFIG" "${APISERVER_CONFIG}.bak"
-
-    # 使用 sed 添加 --bind-address=0.0.0.0
-    sed -i '/spec:\n  containers:\n  - command:/a\    - --bind-address=0.0.0.0' "$APISERVER_CONFIG"
-
-    # 验证修改
-    echo "已添加 --bind-address=0.0.0.0 配置"
-    grep -A 3 "spec:" "$APISERVER_CONFIG"
-fi
-
 
